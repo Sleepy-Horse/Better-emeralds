ab7aa100dfb4cf4aa3d3b3dd3e0bfb0351b3f782
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2022-10-10 01:14:54 +0300

    Adds emerald sword which works

--- src/main/java/content/events/ModEvents.java
+++ src/main/java/content/events/ModEvents.java
@@ -1,29 +1,49 @@
 package content.events;
 
 
+import content.items.EmeraldSword;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import scripts.Main;
 import scripts.registers.ModItems;
 
+import java.util.Objects;
+
 import static net.minecraftforge.fml.common.Mod.*;
 
 @EventBusSubscriber(modid = Main.MODID)
 public class ModEvents {
-    @SubscribeEvent
-    public void MoreDamage(AttackEntityEvent event){
-        Player player = event.getEntity();
-        System.out.println("Checking");
-        if (player.getItemInHand(InteractionHand.MAIN_HAND).is(ModItems.EMERALD_SWORD.get())){
-            System.out.println("You hit with emerald sword!");
-        }
+    public static boolean isFromRaid(LivingEntity entity){
+        if (entity instanceof Pillager)
+            return true;
+        if (entity instanceof Vindicator)
+            return true;
+        if (entity instanceof Ravager)
+            return true;
+        if (entity instanceof Witch)
+            return true;
+        return entity instanceof Evoker;
     }
-
     @SubscribeEvent
-    public void test(LivingEvent.LivingJumpEvent event){
-        System.out.println(event.getEntity());
+    public static void MoreDamage(LivingHurtEvent event) {
+        if (!isFromRaid(event.getEntity())) {
+            return;
+        }
+        if (!(event.getSource().getEntity() instanceof Player player)) {
+            return;
+        }
+        if (!(player.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof EmeraldSword)) {
+            return;
+        }
+        event.getEntity().addEffect(new MobEffectInstance(Objects.requireNonNull(MobEffect.byId(7)), 10), player);
     }
 }
